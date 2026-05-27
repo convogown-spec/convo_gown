0bfb0148b7758943b2774966729fc4e08ad129aa
jibinwilson2004 <jibinwilson.mbits@gmail.com>
2026-05-27 12:02:06 +0530

    feat(seo): update Google Analytics tracking ID to G-D91T1FLX1S

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -181,7 +181,7 @@ export default function RootLayout({
         />
         {/* Dynamic GA4 Tag */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-SEO55CONVO"
+          src="https://www.googletagmanager.com/gtag/js?id=G-D91T1FLX1S"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -189,7 +189,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-SEO55CONVO');
+            gtag('config', 'G-D91T1FLX1S');
           `}
         </Script>
         {/* Microsoft Clarity */}
